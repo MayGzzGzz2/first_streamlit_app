baed2134dda439fa840e56d4c55d3b2f1f99ce08
MayGzzGzz2 <143121798+MayGzzGzz2@users.noreply.github.com>
2023-09-12 10:43:49 -0600

    Update streamlit_app.py
    Snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
-import snowflake.connector
 import streamlit
-  
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -33,6 +31,15 @@ streamlit.dataframe(fruityvice_normalized)
 
 # streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
+
+import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 # write your own comment - what does this do?
 
